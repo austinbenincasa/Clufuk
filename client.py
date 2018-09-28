e727c852acae99bd8895e86da0c857f7ccfa589a
Austin Benincasa <benincasa.austin@gmail.com>
2018-09-28 15:03:01 -0400

    Update client.py

--- client.py
+++ client.py
@@ -4,7 +4,7 @@ import fileTransfer_pb2
 import fileTransfer_pb2_grpc
 
 def main():
-    channel = grpc.insecure_channel('192.168.1.193:50051')
+    channel = grpc.insecure_channel('localhost:50051')
     stub = fileTransfer_pb2_grpc.FileSendStub(channel)
     start = time.time()
     response = stub.SendChunk(chunker())
